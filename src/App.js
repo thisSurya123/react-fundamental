ebb1ee8b405e92472c10d3745e482549ed6b1cc0
thissurya123 <thissurya31@gmail.com>
2024-06-03 18:45:08 +0700

    commite change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:w-3/5 h-3/5 md:m-auto bg-black h-screen text-white">
+
+      <div className="bg-gray-500 p-5">
+        Product Information
+      </div>
+      <div className="p-2">
+        <div className="mb-5">
+          <input className="w-full text-center p-2 bg-gray-900" type="text" placeholder="Search.." />
+        </div>
+        <input name="check" id="check" type="checkbox" />
+        <label for="check">Tampilkan Stock Product Tersisa</label>
+      </div>
+      <ProductTable
+        product={barang}/>
     </div>
   );
 }
+
+function CategoriRow({categori}){
+  return(
+    <tr colspan="2">
+      <td className=" text-center font-bold">{categori}</td>
+    </tr>
+  );
+}
+function ProductTable({product}) {
+  const row = [];
+  let lastCategori = null;
+
+  product.forEach((product) => {
+    if(product.categori !== lastCategori){
+      row.push(
+        <CategoriRow 
+          categori = {product.categori} />
+      );
+    }
+    row.push(
+      <ProductRow 
+        product = {product}/>
+    );
+
+    lastCategori = product.categori;
+  });
+  
+  return(
+    <table className="w-full b-2  border-indigo-200">
+      <tr className="font-bold">
+        <td>Nama</td>
+        <td>harga</td>
+      </tr>
+      {row}
+    </table>
+  );
+}
+function ProductRow({ product }) {
+  const nama = product.stocked ? product.nama :
+    <td className="text-red-500">{product.nama}</td>
+  return (
+    <tr>
+      <td>{nama}</td>
+      <td>{product.harga}</td>
+    </tr>
+  );
+}
+
+const barang = [
+  { categori: "Buah", stocked: true, nama: "Apel", harga: 5000 },
+  { categori: "Buah", stocked: true, nama: "Buah Naga", harga: 20000 },
+  { categori: "Buah", stocked: false, nama: "Pepaya", harga: 10000 },
+  { categori: "Sayuran", stocked: true, nama: "Bayam", harga: 5000 },
+  { categori: "Sayuran", stocked: true, nama: "Tomat", harga: 8000 },
+  { cateogri: "Sayuran", stocked: false, nama: "Cabai", harga: 12000 }
+];
 
 export default App;
